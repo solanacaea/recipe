0f629d622e204aa5840b21fee7d5d85afd2173b0
minLVwang <minLVwang@hotmail.com>
2019-04-23 21:47:04 +0800

    Format

--- Recipe/ui/src/component/CustomMain/ContentMain.js
+++ Recipe/ui/src/component/CustomMain/ContentMain.js
@@ -1,19 +1,16 @@
 import React from 'react'
-//引入路由
 import {Route, Switch} from 'react-router-dom'
+
 import RecipeMain from '../recipe/RecipeMain'
 
 class ContentMain extends React.Component {
     render() {
         return (
-           
-                <Switch>
-                    <Route exact path='/' component={RecipeMain} />
-                    <Route exact path='/recipe' component={RecipeMain}/>
-                   
-                </Switch>
-            
-        )
+            <Switch>
+                <Route exact path='/' component={RecipeMain} />
+                <Route exact path='/recipe' component={RecipeMain}/>
+            </Switch>
+        );
     }
 }
 
