b1ab592493fe799a184772a3457c8d45bebb5eeb
minLVwang <minLVwang@hotmail.com>
2019-04-24 23:16:33 +0800

    Add recipe property model

---
+++ Recipe/ui/src/core/recipe-property.js
@@ -0,0 +1,11 @@
+export default class RecipeProperty {
+  key;
+  label;
+  sourceList;
+
+  constructor(key, label, sourceList) {
+    this.key = key;
+    this.label = label;
+    this.sourceList = sourceList;
+  }
+}
