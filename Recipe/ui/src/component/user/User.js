b1ac3e7b843541d35ff5ed1f349b2ff11dbcb780
solanacaea <xiaofox007@126.com>
2020-03-17 15:02:24 +0800

    add icp 公安局备案号

--- Recipe/ui/src/component/user/User.js
+++ Recipe/ui/src/component/user/User.js
@@ -1,10 +1,13 @@
 import React from 'react'
-import {Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
 import './User.css'
 import LoginForm from './login/Login.Form';
 import RegisterForm from './register/Register.Form';
 import logoURL from '../../assets/img/logo.jpg';
+
+import { Layout } from 'antd';
+const { Content, Footer } = Layout;
 
 export class User extends React.Component {
 
@@ -14,16 +17,25 @@ export class User extends React.Component {
 
     render() {
         return (
-            <div className="container">
-                <div className="logo" onClick={() => this.open()}>
-                    <img alt="心筑月" src={logoURL}/>
-                </div>
-                <div className="main">
-                    <Route component={LoginForm} path='/user/login' exact></Route>
-                    <Route component={RegisterForm} path='/user/register' exact></Route>
-                    <Route component={LoginForm} path='/' exact></Route>
-                </div>
-            </div>
+            <Layout style={{ minHeight: '100vh' }}>
+                 <Content style={{ margin: '0 16px' }}>
+                    <div className="container">
+                        <div className="logo" onClick={() => this.open()}>
+                            <img alt="心筑月" src={logoURL}/>
+                        </div>
+                        <div className="main">
+                            <Route component={LoginForm} path='/user/login' exact></Route>
+                            <Route component={RegisterForm} path='/user/register' exact></Route>
+                            <Route component={LoginForm} path='/' exact></Route>
+                        </div>
+                    </div>
+                </Content>
+                <Footer style={{ textAlign: 'center' }}>
+                    <img  style={{ width: '20px'  }} src="http://img.alicdn.com/tfs/TB1..50QpXXXXX7XpXXXXXXXXXX-40-40.png"/>
+                    <a href="http://beian.miit.gov.cn/"> 沪ICP备20005925号</a>
+                    <span> © 2020-2022 shibonas.cn 版权所有</span>
+                </Footer>
+            </Layout>
         );
     }
 
